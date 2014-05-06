aa4ac2aab5e23bc52f5664cd46aa11a08f76f857
Yuri Lya <yuri-l@users.noreply.github.com>
2014-05-06 15:03:56 +1100

    readme, license.

--- setup.py
+++ setup.py
@@ -6,11 +6,21 @@ from setuptools import setup
 setup(
     name='django-email-helper',
     version = '0.1',
+    packages = ['email_helper'],
     author = 'Yuri Lya',
     author_email = 'yuri.lya@fogstream.ru',
-    maintainer = 'Fogstream',
+    maintainer = 'Fogstream LLC',
     maintainer_email = 'org@fogstream.ru',
-    description = 'The Django-related reusable app provides the ability to send multipart emails and store them in a database.',
     url = 'https://bitbucket.org/fogstream/django-email-helper',
-    packages = ['email_helper'],
+    license = 'The MIT License (MIT)',
+    description = 'The Django-related reusable app provides the ability to send multipart emails and store them in a database.',
+    long_description = open('README.txt').read(),
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
 )
