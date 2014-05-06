d5c77b7a721fa1beedf95570539d02cca69760ef
Yuri Lya <yuri-l@users.noreply.github.com>
2014-05-06 14:31:21 +1100

    Setup.py corrected.

--- email_helper/__init__.py
+++ email_helper/__init__.py
@@ -1,7 +1,5 @@
 # coding=utf-8
 
-__version__ = '0.1'
-
 from os.path import basename
 from smtplib import SMTPException
 
--- setup.py
+++ setup.py
@@ -5,22 +5,12 @@ from setuptools import setup
 
 setup(
     name='django-email-helper',
-    version = __import__('email_helper').__version__,
+    version = '0.1',
     author = 'Yuri Lya',
     author_email = 'yuri.lya@fogstream.ru',
+    maintainer = 'Fogstream'
+    maintainer_email = 'org@fogstream.ru'
     description = 'The Django-related reusable app provides the ability to send multipart emails and store them in a database.',
-    long_description = open('README.rst').read(),
     url = 'https://bitbucket.org/fogstream/django-email-helper',
     packages = ['email_helper'],
-    zip_safe = False,
-    include_package_data = True,
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Framework :: Django",
-        "Topic :: Communications :: Email",
-    ]
 )
