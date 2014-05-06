b1cd9c803df2d796e6056b95794344966565e014
Yuri Lya <yuri-l@users.noreply.github.com>
2014-05-06 14:36:39 +1100

    Setup.py fixed.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     version = '0.1',
     author = 'Yuri Lya',
     author_email = 'yuri.lya@fogstream.ru',
-    maintainer = 'Fogstream'
-    maintainer_email = 'org@fogstream.ru'
+    maintainer = 'Fogstream',
+    maintainer_email = 'org@fogstream.ru',
     description = 'The Django-related reusable app provides the ability to send multipart emails and store them in a database.',
     url = 'https://bitbucket.org/fogstream/django-email-helper',
     packages = ['email_helper'],
