1b331812314bb13c9106cda10de8bb61ed04c4d0
Yuri Lya <yuri-l@users.noreply.github.com>
2014-05-10 18:08:47 +1100

    setup fixed.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 
 setup(
-    name='fogstream-email-helper',
+    name='django-fs-email-helper',
     version = '0.1',
     packages = ['email_helper'],
     include_package_data = True,
     author = 'Yuri Lya',
     author_email = 'yuri.lya@fogstream.ru',
-    url = 'https://bitbucket.org/fogstream/fogstream-email-helper',
+    url = 'https://bitbucket.org/fogstream/django-fs-email-helper',
     license = 'The MIT License (MIT)',
     description = 'The Django-related reusable app provides the ability to send multipart emails and store them in a database.',
     long_description = open('README.txt').read(),
