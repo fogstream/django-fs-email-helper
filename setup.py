98b8edcdcd385f88a07e919d849455f92c64f4ca
Yuri Lya <yuri-l@users.noreply.github.com>
2016-10-25 15:08:17 +1000

    Django 1.9 migrations

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-fs-email-helper',
-    version='0.2.4',
+    version='0.3',
     packages=['email_helper'],
     include_package_data=True,
     install_requires=['html2text'],
